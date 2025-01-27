74eca319a7555b5c4305e5bea994c02f31491e36
3zzazakl <vanzekovichgr@gmail.com>
2025-01-27 05:41:51 +0200

    Solving Front Error

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'primary': '#FFCE1A',
-                'secondary': '#FFA500',
-                'blackBG': '#0F172A',
-                'Favorite': '#FFA500',
-            },
-            fontFamily: {
-                'primary': ["Montserrat", "sans-serif"],
-                'secondary': ["Numito Sans", "sans-serif"],
-            }
-        },
-    },
-    plugins: [],
-}
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
